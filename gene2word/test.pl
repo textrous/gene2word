70de25011eb781dbb4b9db0a4ef53c4bf8a65171
Robin De Schepper <robin.deschepper93@gmail.com>
2021-04-01 12:45:18 +0200

    removed old perl test script

--- gene2word/test.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-
-use warnings;
-use strict;
-use File::Spec;
-
-print "hi!\n\n\n";
-print( (File::Spec->splitpath(File::Spec->rel2abs(__FILE__)))[1] . "\n");
-print "\n\n\nbye!\n";
